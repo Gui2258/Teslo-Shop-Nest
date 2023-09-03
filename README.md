17b4bd2ba0040afd990015c65fe09314ae9e018d
Guido Morffi <guidotele@gmail.com>
2023-09-03 11:43:39 -0400

    Commit inicial

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="nest.svg" width="200" alt="Nest Logo" /></a>
+
+# Teslo API
+
+1. Clonar el proyecto
+
+2. ```yarn install```
+
+3. Crear .env similar a .env.example
+
+4. Levantar la base de datos
+```
+docker-compose up -d
+```
+5. Levantar la aplicación en modo desarrollo
+```
+yarn start:dev
+```
---
+++ src/products/products.service.ts
@@ -0,0 +1,68 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException }
+  from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ){}
+
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+
+    } catch (error) {
+      this.handleDBExeption(error);
+    
+    }
+  }
+
+  async findAll() {
+   return await this.productRepository.find()
+  }
+
+  async findOne(id: string) {
+
+    const product =  await this.productRepository.findOneBy({id});
+
+    if(!product)
+      throw new NotFoundException(`The product with id ${id} not found`)
+
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product  = await this.findOne(id);
+    
+    await this.productRepository.remove(product);
+  }
+
+  private handleDBExeption(error : any){
+
+  if(error.code ==='23505')
+    throw new BadRequestException(error.detail);
+
+  this.logger.error(error);
+  throw new InternalServerErrorException(`Unexpected error, check the server logs`);
+  };
+}
